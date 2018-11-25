80f251b75dcec6b328c0b04ab6e47a7087ccbbd0
EdgarMtz00 <edgar.agustin.martinez@gmail.com>
2018-11-25 13:32:22 -0600

    correccion de errores

--- Carro-Control-IR.ino
+++ Carro-Control-IR.ino
@@ -1,21 +1,16 @@
 #include <IRremote.h>
 
-const int pinRec=13;
+int pinRec=13;
 
 IRrecv receptor(pinRec);
 decode_results *resultados;
 
 void setup(){
 	Serial.begin(9600);
-	repceptor.enableIRIn();
+	receptor.enableIRIn();
 	Serial.println("Receptor habilitado...");
 }
 
 void loop(){
-	if(receptor.decode(&resultados)){
-		Serial.println(resultados.value,HEX);
-		receptor.resume();
-	}
-	delay(500);
 	
 }
--- ScriptObtenerCodigo/ScriptObtenerCodigo.ino
+++ ScriptObtenerCodigo/ScriptObtenerCodigo.ino
@@ -1,13 +1,13 @@
 #include <IRremote.h>
 
-const int pinRec=13;
+int pinRec=13;
 
 IRrecv receptor(pinRec);
 decode_results *resultados;
 
 void setup(){
 	Serial.begin(9600);
-	repceptor.enableIRIn();
+	receptor.enableIRIn();
 	Serial.println("Receptor habilitado...");
 }
 
