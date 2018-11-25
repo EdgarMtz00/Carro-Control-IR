4b2a2c5f128d2e8abaeba2df1c36954536ea530a
EdgarMtz00 <edgar.agustin.martinez@gmail.com>
2018-11-25 17:50:27 -0600

    Agrega variables para control de los motores y funcion de encender y apagar sin beep

--- Carro-Control-IR.ino
+++ Carro-Control-IR.ino
@@ -1,16 +1,48 @@
 #include <IRremote.h>
 
-int pinRec=13;
+const int pinRec=13;
+const int pinVel=1;
+const int M1Izq=3;	//avanzar
+const int M1Der=4;	//retroceder
+const int M2Izq=5;	//avanzar
+const int M2Der=6;	//retroceder
 
 IRrecv receptor(pinRec);
 decode_results resultados;
+unsigned long valorPrev;
+
+bool encendido = false;
+int velocidades[4] = {141, 179, 217, 255};
 
 void setup(){
 	Serial.begin(9600);
 	receptor.enableIRIn();
 	Serial.println("Receptor habilitado...");
+	digitalWrite(M1Izq,LOW);
+	digitalWrite(M1Der,LOW);
+	digitalWrite(M2Izq,LOW);
+	digitalWrite(M2Der,LOW);
+}
+
+void encender(){
+	if(encendido){
+		digitalWrite(M1Izq,LOW);
+		digitalWrite(M1Der,LOW);
+		digitalWrite(M2Izq,LOW);
+		digitalWrite(M2Der,LOW);	
+	}
+	encendido = !encendido;
+	analogWrite(pinVel, velocidades[0]);
 }
 
 void loop(){
-	
+	if(receptor.decode(&resultados)){
+		if(resultado.value == 0XFFFFFF){
+			reultado.value = valorPrev; 
+		}
+		switch(resultado.value){
+			case 0xFFA25D: //Arrancar/Apagar
+				encender();
+		}
+	}
 }
