5c9c00c5ade6b1c206cee47e2329584b4ca7a03f
EdgarMtz00 <edgar.agustin.martinez@gmail.com>
2018-11-26 21:25:58 -0600

    incluye cancion del claxon y prueba en texto del carro

--- PruebaCarroTexto/PruebaCarroTexto.ino
+++ PruebaCarroTexto/PruebaCarroTexto.ino
@@ -1,5 +1,4 @@
 #include <IRremote.h>
-#include "pitches.h"
 
 const int pinRec = 13;
 const int pinVel = 1;
@@ -9,8 +8,6 @@ const int M2Izq = 5;	//avanzar		izquierda
 const int M2Der = 6;	//retroceder
 const int pinBocina = 7;
 
-int claxon[]={NOTE_C4, NOTE_C4, NOTE_C4, NOTE_F4, NOTE_A4, 0, NOTE_C4, NOTE_C4, NOTE_C4, NOTE_F4, NOTE_A4};
-int claxonRitmo[]={8,8,8,2,2,4,8,8,8,2,2};
 
 IRrecv receptor(pinRec);
 decode_results resultados;
@@ -26,53 +23,49 @@ void setup(){
 	Serial.begin(9600);
 	receptor.enableIRIn();
 	Serial.println("Receptor habilitado...");
-
-	digitalWrite(M1Izq,LOW);
+	Serial.println("Inicializado");
+	/*digitalWrite(M1Izq,LOW);
 	digitalWrite(M1Der,LOW);
 	digitalWrite(M2Izq,LOW);
-	digitalWrite(M2Der,LOW);
-}
-
-Void pitar(){
-	for (int i = 0; i < 11; ++i)
-	{
-		tone(pinBocina, claxon[i], 1000/claxonRitmo[i]);
-		delay((1000/claxonRitmo)*1.30);
-		noTone(pinBocina);
-	}
+	digitalWrite(M2Der,LOW);*/
 }
 
 void encender(){
-	if(encendido){
+	Serial.println("encendido");
+	/*if(encendido){
 		digitalWrite(M1Izq,LOW);
 		digitalWrite(M1Der,LOW);
 		digitalWrite(M2Izq,LOW);
 		digitalWrite(M2Der,LOW);	
 	}
+	analogWrite(pinVel, velocidades[0]);*/
 	encendido = !encendido;
-
-	analogWrite(pinVel, velocidades[0]);
+  Serial.println(encendido);
 }
 
 void arrancar(){
 	if(encendido){
 		if(marcha){
-			digitalWrite(M1Izq,LOW);
+			/*digitalWrite(M1Izq,LOW);
 			digitalWrite(M1Der,LOW);
 			digitalWrite(M2Izq,LOW);
-			digitalWrite(M2Der,LOW);
+			digitalWrite(M2Der,LOW);*/
+			Serial.println("Detenido");
 			marcha = false;
 		}else if(!reversa){
+			/*
 			digitalWrite(M1Izq,HIGH);
 			digitalWrite(M1Der,LOW);
 			digitalWrite(M2Izq,HIGH);
-			digitalWrite(M2Der,LOW);
+			digitalWrite(M2Der,LOW);*/
+			Serial.println("andando");
 			marcha = true;
 		}else if(reversa){
-			digitalWrite(M1Izq,LOW);
+			/*digitalWrite(M1Izq,LOW);
 			digitalWrite(M1Der,HIGH);
 			digitalWrite(M2Izq,LOW);
-			digitalWrite(M2Der,HIGH);
+			digitalWrite(M2Der,HIGH);*/
+			Serial.println("andando de reversa");
 			marcha = true;
 		}
 	}
@@ -80,41 +73,54 @@ void arrancar(){
 
 void subirVel(){
 	if(encendido & vel<3){
-		vel++
+		vel++;
 	}
-	analogWrite(pinVel, velocidades[vel]);
-}
+	//analogWrite(pinVel, velocidades[vel]);
+  Serial.print("velocidad: ");
+  Serial.println(vel);
+ }
 
 void bajarVel(){
 	if(encendido & vel!=0){
 		vel--;	
 	}
-	analogWrite(pinVel, velocidades[vel]);
+	//analogWrite(pinVel, velocidades[vel]);
+	Serial.print("velocidad: ");
+	Serial.println(vel);
 }
 
 void girar(bool dir){ //0 derecha | 1 izquierda
 	if(encendido){
 		if(!reversa){
-			digitalWrite(M1Izq, !dir);
-			digitalWrite(M2Izq, dir);
+			/*digitalWrite(M1Izq, !dir);
+			digitalWrite(M2Izq, dir);*/
+			Serial.print("giro a:");
 		}else if(reversa){
-			digitalWrite(M1Der, dir);
-			digitalWrite(M2Der, !dir);
+			/*digitalWrite(M1Der, dir);
+			digitalWrite(M2Der, !dir);*/
+			Serial.print("giro de reversa a:");
 		}
+    if(dir){
+        Serial.println("izquierda");
+      }else{
+        Serial.println("derecha");
+      }
 		delay(100);
 		receptor.resume();
-		if (receptor.decode_results(&resultados))
+		if (receptor.decode(&resultados))
 		{
 			if (resultados.value==valorPrev || resultados.value==0xFFFFFFFF)
 			{
 				girar(dir);
 			}else{
 				if(!reversa){
-					digitalWrite(M1Izq, HIGH);
-					digitalWrite(M2Izq, HIGH);
+					/*digitalWrite(M1Izq, HIGH);
+					digitalWrite(M2Izq, HIGH);*/
+					Serial.println("dejo de girar");
 				}else if(reversa){
-					digitalWrite(M1Der, HIGH);
-					digitalWrite(M2Der, HIGH);
+					/*digitalWrite(M1Der, HIGH);
+					digitalWrite(M2Der, HIGH);*/
+					Serial.println("dejo de girar y retrocedio");
 				}
 				loop();
 			}
@@ -124,21 +130,26 @@ void girar(bool dir){ //0 derecha | 1 izquierda
 }
 
 void cambiarSentido(bool dir){ //0 adelante | 1 atras
-	if (encendido){
+	if (encendido){/*
 		digitalWrite(M1Der, dir);
 		digitalWrite(M1Izq, !dir);
 		digitalWrite(M2Der, dir);
-		digitalWrite(M2Izq, !dir);
+		digitalWrite(M2Izq, !dir);*/
 		reversa = dir;
+		if (reversa){
+			Serial.println("Reversa");
+		}else{
+			Serial.println("andando");
+		}
 	}
 }
 
 void loop(){
 	if(receptor.decode(&resultados)){
 		if(resultados.value == 0XFFFFFFFF){
-			reultados.value = valorPrev; 
+			resultados.value = valorPrev; 
 		}
-		valorPrev = resultado.value;
+		valorPrev = resultados.value;
 		switch(resultados.value){
 			case 0xFFA25D: //Power
 				encender();
@@ -165,12 +176,12 @@ void loop(){
 				cambiarSentido(1);
 				break;
 			case 0xFFE21D: //Func
-				pitar();
+				//pitar();
 				break;
 			default:
 				break;
 		}
-		delay(500);
+   delay(500);
 		receptor.resume();
 	}
 }
